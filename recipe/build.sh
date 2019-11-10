389a71d6fd2720536aea495a290363784d2db0c1
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-11-10 11:19:24 +0100

    Add tempest build matrix

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,11 @@ set -e
 set -x
 
 if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
-  export CONFIGURE_ARGS="CC=mpicc CXX=mpic++ F77=mpif77 FC=mpif90 F90=mpif90--with-mpi=${PREFIX} ${CONFIGURE_ARGS}"
+  export CONFIGURE_ARGS="CC=mpicc CXX=mpic++ F77=mpif77 FC=mpif90 F90=mpif90 --with-mpi=${PREFIX} ${CONFIGURE_ARGS}"
+fi
+
+if [[ ! -z "$tempest" && "$tempest" != "notempest" ]]; then
+  export CONFIGURE_ARGS="--with-tempestremap=${PREFIX} --with-eigen3=${PREFIX} ${CONFIGURE_ARGS}"
 fi
 
 autoreconf -fi
