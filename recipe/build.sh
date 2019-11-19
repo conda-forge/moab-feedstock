bd426e59892e3b0ece342c3b2b6ef441424ad2a4
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-11-19 23:41:56 +0100

    Rebuild with TempestRemap 2.0.2
    This version of moab is not compatible with 2.0.3, the latest
    release.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,12 @@
 set -e
 set -x
 
-if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
+if [[ -n "$mpi" && "$mpi" != "nompi" ]]; then
   export CONFIGURE_ARGS="CC=mpicc CXX=mpic++ F77=mpif77 FC=mpif90 F90=mpif90 --with-mpi=${PREFIX} ${CONFIGURE_ARGS}"
 fi
 
-if [[ ! -z "$tempest" && "$tempest" != "notempest" ]]; then
-  export CONFIGURE_ARGS="--with-tempestremap=${PREFIX} --with-eigen3=${PREFIX} ${CONFIGURE_ARGS}"
+if [[ -n "$tempest" && "$tempest" != "notempest" ]]; then
+  export CONFIGURE_ARGS="--with-tempestremap=${PREFIX} --with-eigen3=${PREFIX}/include/eigen3 --with-netcdf=${PREFIX} ${CONFIGURE_ARGS}"
 fi
 
 autoreconf -fi
