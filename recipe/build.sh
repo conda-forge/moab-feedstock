9e01801363d7eec4d42baf95886694496f47e7d5
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-01-24 14:13:12 -0700

    Disable Fortran (not needed without BLAS/LAPACK)

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -x
 export CONFIGURE_ARGS="--with-eigen3=${PREFIX}/include/eigen3 ${CONFIGURE_ARGS}"
 
 if [[ -n "$mpi" && "$mpi" != "nompi" ]]; then
-  export CONFIGURE_ARGS="CC=mpicc CXX=mpic++ F77=mpif77 FC=mpif90 F90=mpif90 --with-mpi=${PREFIX} ${CONFIGURE_ARGS}"
+  export CONFIGURE_ARGS="CC=mpicc CXX=mpic++ --with-mpi=${PREFIX} ${CONFIGURE_ARGS}"
 fi
 
 if [[ -n "$tempest" && "$tempest" != "notempest" ]]; then
@@ -19,6 +19,7 @@ autoreconf -fi
   --enable-shared \
   --enable-tools \
   --enable-pymoab \
+  --disable-fortran \
   || { cat config.log; exit 1; }
 make -j "${CPU_COUNT}"
 if [ "$(uname)" == "Linux" ]; then
