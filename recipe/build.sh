88dcd87076dc7c5234e890e01dbfba4ccfa9fc96
Paul P.H. Wilson <paul.wilson@wisc.edu>
2021-08-22 18:58:31 -0500

    Switch to CMake

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,25 +3,26 @@ set -e
 set -x
 
 export CONFIGURE_ARGS="--with-eigen3=${PREFIX}/include/eigen3 ${CONFIGURE_ARGS}"
+export CONFIGURE_ARGS="-DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX}"
 
 if [[ -n "$mpi" && "$mpi" != "nompi" ]]; then
-  export CONFIGURE_ARGS="CC=mpicc CXX=mpic++ --with-mpi=${PREFIX} --with-zoltan="${PREFIX}" ${CONFIGURE_ARGS}"
+  export CONFIGURE_ARGS="-DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpic++ -DENABLE_MPI=ON -DENABLE_ZOLTAN=ON"
 fi
 
 if [[ -n "$tempest" && "$tempest" != "notempest" ]]; then
-  export CONFIGURE_ARGS="--with-tempestremap=${PREFIX} --with-netcdf=${PREFIX} ${CONFIGURE_ARGS}"
+  export CONFIGURE_ARGS="-DENABLE_TEMPESTREMAP=ON -DENABLE_NETCDF=ON ${CONFIGURE_ARGS}"
 fi
 
-autoreconf -fi
-./configure --prefix="${PREFIX}" \
-  ${CONFIGURE_ARGS} \
-  --with-hdf5="${PREFIX}" \
-  --with-metis="${PREFIX}" \
-  --enable-shared \
-  --enable-tools \
-  --enable-pymoab \
-  --disable-fortran \
-  || { cat config.log; exit 1; }
+mkdir bld
+cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+         -DENABLE_HDF5=ON \
+         -DHDF5_ROOT=${PREFIX} \
+         -DENABLE_METIS=ON \
+         -DBUILD_SHARED_LIBS=ON \
+         -DENABLE_PYMOAB=ON \
+         -DENABLE_BLASLAPACK=OFF \
+         -DENABLE_FORTRAN=OFF \
+         ${CONFIGURE_ARGS}
 
 make -j "${CPU_COUNT}"
 
